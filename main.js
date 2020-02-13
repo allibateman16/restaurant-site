ed5aa7499d2f15f59c8eafcc1b599d130987fe1a
Alli Bateman <allibateman@Allis-MacBook-Pro.local>
2020-02-13 12:31:03 +0100

    modal working, vegitarian, alcohol, and allergens are not

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+const modal = document.querySelector(".modal-background");
+modal.addEventListener("click", () => {
+  modal.classList.add("hide");
+});
+
 fetch("https://kea-alt-del.dk/t5/api/categories")
 .then(function (response){
       return response.json()
@@ -41,10 +46,7 @@ fetch("https://kea-alt-del.dk/t5/api/productlist")
     }
 
 function showData(jsonData){
-    console.log(jsonData)
-
     jsonData.forEach(showSingleDish)
-}
 
 function showSingleDish(dish){
     console.log(dish)
@@ -61,7 +63,7 @@ aCopy.querySelector("img").src = "https://kea-alt-del.dk/t5/site/imgs/" + "small
 //append somewhere
 const showMe = document.querySelector("main");
 
-
+//Discounted items
 if (dish.discount) {
     aCopy.querySelector(".priceDiscount span").textContent = dish.price = Math.round(dish.price - dish.price * dish.discount / 100);
 }
@@ -70,16 +72,65 @@ else{
     aCopy.querySelector(".priceDiscount").remove()
     aCopy.querySelector(".fullPrice span").textContent = dish.price
 }
+
+
+
+
+//Sold out
+if (dish.soldout) {
+    aCopy.querySelector(".soldOut").style.display = "block"
+}
+
+
+//modal
+  aCopy.querySelector("button").addEventListener("click", () => {
+      console.log("click", dish)
+    fetch(`https://kea-alt-del.dk/t5/api/product?id=${dish.id}`)
+      .then(res => res.json())
+      .then(showDetails);
+  });
+
+function showDetails(data) {
+  modal.querySelector(".modal-name").textContent = data.name;
+modal.querySelector(".modal-image");
+  modal.querySelector(".modal-description").textContent = data.longdescription;
+modal.querySelector(".modal-price>span").textContent = data.price = Math.round(data.price - data.price * data.discount / 100);
+
+    //modal vegitarian
+if (data.vegetarian) {
+    modal.querySelector(".vegetarian").textContent = data.vegetarian;
+}
+
+//modal allergens
+if (data.allergens) {
+    modal.querySelector(".allergens").textContent = dish.allergens;
+}
+
+//modal alcohol
+if (data.alcohol) {
+    modal.querySelector(".alcohol").textContent =
+        dish.alcohol;
+}
+
+    modal.classList.remove("hide");
+}
+
+
+
 //create an id out of data
 console.log(`#${dish.category}`)
 document.querySelector(`#${dish.category}`).appendChild(aCopy)
 
-/*if (dish.soldout) {
-    aCopy.querySelector(".allGone").textContent = "We are all out!";
-}*/
-
-
 showMe.appendChild(aCopy);
 }
+
+
+}
+
+
+
+
+
+
 
 
